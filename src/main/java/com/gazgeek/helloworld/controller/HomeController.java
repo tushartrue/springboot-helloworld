eb96001e74844d9eeed3432a16e9fedaa8df8a56
tushartrue <tushartrue@gmail.com>
2022-03-26 15:56:37 +0530

    Update HomeController.java

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from Tushar from Pune 1!";
+        return "Hello from Tushar,Shiv and Poonam!";
     }
 
 }
